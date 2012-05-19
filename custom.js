8cd9ff9c4f6648ac74d0246a426cb0742e5dc05f
Allard van Helbergen <allard@vanhelbergen.com>
2012-05-19 17:00:03 +0100

    Adding link to twitter profile for each tweet. Refactored code to have a seperate function for AJAX succes.

--- custom.js
+++ custom.js
@@ -14,36 +14,37 @@ function callTwitter(event) {
       q: $('#query').val()
     },
     success: function(data, textStatus, xhr) {
-      var result = $('<div>').addClass('result');
-      console.log(data);
-      for (var i = 0, tweet; tweet = data.results[i]; i++) {
-        var row = $('<div>').addClass('row');
-        row.append(
-          $('<div>').addClass('span1 offset1').append(
-            $('<img>').attr('src', tweet.profile_image_url)
-          ).append($('<br>')).append(
-            $('<p>').html(tweet.from_user)
-          )
-        ).append(
-          $('<div>').addClass('span5 offset1').append(
-            $('<p>').append(
-              $('<strong>').html(tweet.text)
-            )
-          )
-        ).append(
-          $('<div>').addClass('span3 offset1').append(
-            $('<p>').append(
-              $('<em>').html(tweet.created_at)
-            )
-          ).append(
-            $('<p>').html(tweet.iso_language_code)
-          ).append(
-            $('<p>').html(tweet.geo)
-          )
-        );
-        $(result).append(row);
-      }
-      $('.result').replaceWith(result);
+      parseTweets(data);
     }
   });
+}
+
+function parseTweets(tweets) {
+  var result = $('<div>').addClass('result');
+
+  for (var i = 0, tweet; tweet = tweets.results[i]; i++) {
+    var row = $('<div>').addClass('row');
+
+    row.append(
+        $('<div>').addClass('span1 offset1').append(
+            $('<p>').append(
+                $('<a>').attr('href', 'http://twitter.com/' 
+                              + tweet.from_user).append(
+                    $('<img>').attr('src', tweet.profile_image_url))).append(
+                $('<a>').attr('href', 'http://twitter.com/'
+                              + tweet.from_user)
+                    .html(tweet.from_user_name)))).append(
+        $('<div>').addClass('span5 offset1').append(
+            $('<p>').append(
+                $('<strong>').html(tweet.text)))).append(
+        $('<div>').addClass('span3 offset1').append(
+            $('<p>').append(
+                $('<em>').html(tweet.created_at))).append(
+            $('<p>').html(tweet.iso_language_code)).append(
+            $('<p>').html(tweet.geo)));
+
+    $(result).append(row);
+  }
+
+  $('.result').replaceWith(result);
 }
